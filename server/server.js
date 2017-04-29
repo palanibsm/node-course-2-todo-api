22f324c2266c73720260db1e4b32adcc1524fb29
senthil2world@gmail.com <senthil2world@gmail.com>
2017-04-29 22:41:53 +0800

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParse = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -20,16 +21,37 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
 
+//GET all Todos
 app.get('/todos/', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET Todo by Id
+app.get('/todos/:id', (req, res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    res.status(404).send('ObjectID is Invalid');
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo)
+    {
+      res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+// .catch ((e) => console.log(e));
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
