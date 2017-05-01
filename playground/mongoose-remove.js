a6178409147485cdfa6e5b93f8a36d7cc8d9b3b4
senthil2world@gmail.com <senthil2world@gmail.com>
2017-05-01 15:56:40 +0800

    Added Server config file

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+//Todo.findOneAndRemove
+//Todo.findByIdAndRemove
+var id = "5905e2e73fec0d1406dcab1c";
+
+Todo.findByIdAndRemove(id).then((todo) => {
+  console.log(todo);
+});
