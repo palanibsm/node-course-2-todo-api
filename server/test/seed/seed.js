254b1391d1a80578360de0ffa4bdc5437d20d7c5
senthil2world@gmail.com <senthil2world@gmail.com>
2017-05-09 00:33:51 +0800

    Add tests for GET /users/me and POST /users

---
+++ server/test/seed/seed.js
@@ -0,0 +1,50 @@
+const {ObjectID} = require('mongodb');
+var {Todo} = require('./../../models/todo');
+var {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+require('./../../config/config.js');
+
+var todos = [{
+  _id: new ObjectID(),
+  text: 'First test Todo...'
+  }, {
+  _id: new ObjectID(),
+  text: 'Second test Todo...',
+  completed: true,
+  completedAt: 333
+}];
+
+var userOneId = new ObjectID();
+var userTwoId = new ObjectID();
+
+var users = [{
+  _id: userOneId,
+  email: 'staff2@rydesharing.com',
+  password: 'userOnePass',
+  tokens: [{
+      'access': 'auth',
+      'token': jwt.sign({_id: userOneId, access: 'auth'}, process.env.saltSecret).toString()
+    }
+  ]
+},{
+  _id: userTwoId,
+  email: 'staff1@rydesharing.com',
+  password: 'userTwoPass'
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,22 +5,12 @@ const {ObjectID} = require('mongodb');
 
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
+var {User} = require('./../models/user');
 
-var todos = [{
-  _id: new ObjectID(),
-  text: 'First test Todo...'
-  }, {
-  _id: new ObjectID(),
-  text: 'Second test Todo...',
-  completed: true,
-  completedAt: 333
-}];
+var {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-      return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('Should create a new Todo', (done) => {
@@ -184,4 +174,90 @@ describe('PATCH /todos:id', () => {
       .end(done)
   });//it ends
 
+});
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('shoud return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    var email = 'bsm@ryde.com';
+    var password = 'pass123!!';
+    request(app)
+      .post('/users')
+      .send({
+        email,
+        password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err) => {
+        if (err){
+          return done(err);
+        }
+
+        User.findOne({email}).then((user) => {
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        })
+      });
+  });
+
+  it('should return a validation if request have errors', (done) => {
+    var email = 'abc@ryde.com';
+    var password = '123';
+    request(app)
+      .post('/users')
+      .send({
+        email,
+        password
+      })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email is in use', (done) => {
+    var email = users[0].email;
+    var password = 'pass123!!';
+    request(app)
+      .post('/users')
+      .send({
+        email,
+        password
+      })
+      .expect(400)
+      .end((err) => {
+        if (err){
+          return done(err);
+        }
+        done();
+      });
+
+  });
 });
