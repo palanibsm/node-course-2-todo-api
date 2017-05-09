38bfd212a4b501777fc71a61ff7acff512623407
senthil2world@gmail.com <senthil2world@gmail.com>
2017-05-10 00:21:49 +0800

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,27 @@ UserSchema.statics.findByToken = function (token) {
   })
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        // console.log(res);
+        // console.log(password);
+        // console.log(user.password);
+
+        if (!res){
+          reject();
+        }
+        resolve(user);
+      })
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
