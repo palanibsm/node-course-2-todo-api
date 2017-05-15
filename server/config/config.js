d750d8f01ffac8a33539167ff74af8776ed18942
senthil2world@gmail.com <senthil2world@gmail.com>
2017-05-15 23:18:04 +0800

    Update Config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  // console.log(config);
+  var envConfig = config[env];
+  // console.log(Object.keys(envConfig));
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 };
 
-process.env.saltSecret = "abc123";
+// if (env === 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// };
+
+//process.env.saltSecret = "abc123";
--- server/models/user.js
+++ server/models/user.js
@@ -44,7 +44,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function (){
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.saltSecret).toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   // console.log(token);
   user.tokens.push({access, token});
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, process.env.saltSecret);
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject('Authentication Failed');
   }
