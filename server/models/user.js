7ce95adfc6396d35af3a75163e06bac7a6216e46
senthil2world@gmail.com <senthil2world@gmail.com>
2017-05-16 22:57:01 +0800

    heroku debug4

--- server/models/user.js
+++ server/models/user.js
@@ -44,6 +44,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function (){
   var user = this;
   var access = 'auth';
+  console.log('before jwt');
   var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   console.log(token);
